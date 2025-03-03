0b3eba03c30008cfc341f1eb50a13c15e22158b5
timofey-timofey <kotyubeev2006@gmail.com>
2025-03-03 13:26:52 +0700

    Add files via upload

---
+++ pz2.py
@@ -0,0 +1,46 @@
+class PostalAddress:
+    def __init__(self, street, house, apartment):
+        self._street = street
+        self._house = house
+        self._apartment = apartment
+
+    @property
+    def street(self):
+        return self._street
+
+    @street.setter
+    def street(self, value):
+        self._street = value
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, value):
+        self._house = value
+
+    @property
+    def apartment(self):
+        return self._apartment
+
+    @apartment.setter
+    def apartment(self, value):
+        self._apartment = value
+
+# Создаем экземпляры класса
+address1 = PostalAddress("Ленина", 12, 34)
+address2 = PostalAddress("Пушкина", 5, 10)
+
+# Выводим значения свойств объектов
+print(f"Адрес 1: Улица - {address1.street}, Дом - {address1.house}, Квартира - {address1.apartment}")
+print(f"Адрес 2: Улица - {address2.street}, Дом - {address2.house}, Квартира - {address2.apartment}")
+
+# Изменяем значения полей
+address1.street = "Карла Маркса"
+address2.apartment = 15
+
+# Выводим обновленные адреса
+print(f"Обновленный Адрес 1: Улица - {address1.street}, Дом - {address1.house}, Квартира - {address1.apartment}")
+print(f"Обновленный Адрес 2: Улица - {address2.street}, Дом - {address2.house}, Квартира - {address2.apartment}")
+
